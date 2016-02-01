f4f34c917178b200044840ba5928b5474393005d
Alexey <vitkovskiya@gmail.com>
2016-02-01 14:44:02 +0300

    Added file output

--- OnlinerParcer/Program.cs
+++ OnlinerParcer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,21 +11,36 @@ namespace OnlinerParcer
 {
     class Program
     {
+        private static void PrintToFile(List<Auto> autos)
+        {
+            using (var stream = File.CreateText(@"..\..\result.txt"))
+            {
+                foreach (var auto in autos)
+                {
+                    stream.WriteLine("Id: {0}, Name: {1}", auto.Id, auto.Name);
+                    foreach (var phoneNumber in auto.PhoneNumbers)
+                    {
+                        stream.WriteLine(" Phone: {0}", phoneNumber);
+                    }
+                    stream.WriteLine();
+                }
+            }
+            //foreach (var auto in autos)
+            //{
+            //    Console.WriteLine("Id: {0}, Name: {1}", auto.Id, auto.Name);
+            //    foreach (var phoneNumber in auto.PhoneNumbers)
+            //    {
+            //        Console.WriteLine(" Phone: {0}", phoneNumber);
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Thread:{0}", Thread.CurrentThread.ManagedThreadId);
             var parcer = new AutoBaraholkaParcer();
             var autos = parcer.Parce();
-            foreach (var auto in autos)
-            {
-                Console.WriteLine("Id: {0}, Name: {1}", auto.Id, auto.Name);
-                foreach (var phoneNumber in auto.PhoneNumbers)
-                {
-                    Console.WriteLine(" Phone: {0}", phoneNumber);
-                }
-                Console.WriteLine();
-            }
+            PrintToFile(autos);
         }
     }
 }
